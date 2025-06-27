2b33a4dc144031c6bb51a15c94ab7679c31bf7e8
AlexeyShipovalov <dappi@DESKTOP-OSJ7MGA.>
2025-06-27 21:14:05 +0600

    Задача на палиндром

---
+++ task01/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+            string cleanText = "";
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    cleanText += char.ToLower(c);
+                }
+            }
+            int left = 0;
+            int right = cleanText.Length - 1;
+            while (left < right)
+            {
+                if (cleanText[left] != cleanText[right])
+                    return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
