12163cfb29d0ef6ffb2c2f1a1064a2a371a0e54b
AlexeyShipovalov <dappi@DESKTOP-OSJ7MGA.>
2025-07-09 18:43:51 +0600

    task10

---
+++ PluginAttributes/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PluginAttributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PluginLoadAttribute : Attribute
+    {
+        public string Name { get; }
+        public PluginLoadAttribute(string name) => Name = name;
+    }
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class DependsOnAttribute : Attribute
+    {
+        public string PluginName { get; }
+        public DependsOnAttribute(string pluginName) => PluginName = pluginName;
+    }
+}
---
+++ PluginLoader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PluginLoader; 
+namespace PluginLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Укажите путь к папке с плагинами");
+                return;
+            }
+            var plugins = PluginCore.LoadFromDirectory(args[0]);
+            plugins.ExecuteAll();
+        }
+    }
+}
