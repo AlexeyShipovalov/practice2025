d636269a965c855825f6749796f8f4fbd0090f1e
AlexeyShipovalov <dappi@DESKTOP-OSJ7MGA.>
2025-07-09 22:46:56 +0600

    исправлен путь для получения расположения dll

--- task10tests/UnitTest1.cs
+++ task10tests/UnitTest1.cs
@@ -2,20 +2,26 @@ using Xunit;
 using System;
 using System.IO;
 using PluginLoader;
+using System.Reflection;
 namespace task10tests
 {
     public class PluginLoadingTests : IDisposable
     {
-        private readonly string _pluginsDir = Path.Combine(Directory.GetCurrentDirectory(), "test_plugins");
+        private readonly string _pluginsDir;
+
         public PluginLoadingTests()
         {
+            _pluginsDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "test_plugins");
             Directory.CreateDirectory(_pluginsDir);
-            string lib1Path = "/home/dappi/practice2025/practice2025/PluginLibrary1/bin/Debug/net8.0/PluginLibrary1.dll";
-            string lib2Path = "/home/dappi/practice2025/practice2025/PluginLibrary2/bin/Debug/net8.0/PluginLibrary2.dll";
+            var solutionDir = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), 
+                "../../../../"));
+            var lib1Path = Path.Combine(solutionDir, "PluginLibrary1/bin/Debug/net8.0/PluginLibrary1.dll");
+            var lib2Path = Path.Combine(solutionDir, "PluginLibrary2/bin/Debug/net8.0/PluginLibrary2.dll");
             if (!File.Exists(lib1Path))
-                throw new FileNotFoundException($"Файл не найден: {lib1Path}");
+                throw new FileNotFoundException($"PluginLibrary1.dll не найден в: {lib1Path}");
             if (!File.Exists(lib2Path))
-                throw new FileNotFoundException($"Файл не найден: {lib2Path}");
+                throw new FileNotFoundException($"PluginLibrary2.dll не найден в : {lib2Path}");
             File.Copy(lib1Path, Path.Combine(_pluginsDir, "PluginLibrary1.dll"), true);
             File.Copy(lib2Path, Path.Combine(_pluginsDir, "PluginLibrary2.dll"), true);
         }
