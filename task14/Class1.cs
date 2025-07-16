38dd82ca1430fe5a4354681469e1df071c370869
AlexeyShipovalov <dappi@DESKTOP-OSJ7MGA.>
2025-07-16 17:50:49 +0600

    task15

--- task14/Class1.cs
+++ task14/Class1.cs
@@ -2,7 +2,7 @@
 using System.Threading;
 namespace task14
 {
-    public class DefiniteIntegral
+    public static class DefiniteIntegral
     {
         private static object lockObj = new object();
         public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
---
+++ task15/Program.cs
@@ -0,0 +1,8 @@
+﻿using task14;
+class Program
+{
+    static void Main()
+    {
+        task14.Integral.Main();
+    }
+}
